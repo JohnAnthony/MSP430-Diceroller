90883c066874b3cbe224bb1fde42c81d9a837f6b
John Anthony <johnanthony@lavabit.com>
2013-02-02 02:25:29 +0000

    First commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+ARCH	:= msp430
+CHIP	:= g2211
+CC	    := $(ARCH)-gcc
+OBJCOPY := $(ARCH)-objcopy
+OBJDUMP := $(ARCH)-objdump
+
+MCU	    := -mmcu=$(ARCH)$(CHIP)
+CFLAGS  := $(MCU) -O2 -Wall
+OBJECTS := roller.o
+
+.PHONY: clean
+
+all: $(OBJECTS)
+	$(CC) $(MCU) -o roller.elf roller.o
+	$(OBJCOPY) -O ihex roller.elf roller.a43
+	$(OBJDUMP) -dSt roller.elf > roller.lst
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+clean:
+	rm roller.a43 roller.lst roller.elf $(OBJECTS)
---
+++ roller.c
@@ -0,0 +1,40 @@
+#include <msp430g2211.h>
+
+#define LENGTH(x) (sizeof(x) / sizeof(x[0]))
+
+static unsigned int DICE_DISPLAYS[] = {
+    BIT3,                                       /* One */
+    BIT1 | BIT5,                                /* Two */
+    BIT1 | BIT3 | BIT5,                         /* Three */
+    BIT0 | BIT1 | BIT5 | BIT6,                  /* Four */
+    BIT0 | BIT1 | BIT3 | BIT5 | BIT6,           /* Five */
+    BIT0 | BIT1 | BIT2 | BIT4 | BIT5 | BIT6     /* Six */
+};
+
+/* Busy wait */
+/* TODO: Use signals */
+void wait(unsigned int d) {
+    for (; d>0; d--)
+        nop();
+}
+
+int main(void) {
+    int dicevalue;
+
+    WDTCTL = WDTPW + WDTHOLD;
+    P1DIR  = BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6;
+    P2DIR  = 0x01;
+
+    dicevalue = 0;
+    while (1) {
+        P1OUT = DICE_DISPLAYS[dicevalue];
+
+        dicevalue++;
+        if (dicevalue >= LENGTH(DICE_DISPLAYS))
+            dicevalue = 0;
+
+        wait(0xffff);
+        wait(0xffff);
+        wait(0xffff);
+    }
+}
